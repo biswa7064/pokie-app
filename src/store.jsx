de64d2a946f5fea70011142fca7fe429465444c5
biswa7064 <biswaranjan16856@gmail.com>
2021-01-21 21:30:46 +0530

    Adding store and reducer

---
+++ src/store.jsx
@@ -0,0 +1,10 @@
+import {createStore} from 'redux'
+import {applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import RootReducer from './reducers/RootReducer'
+
+
+
+const store = createStore(RootReducer, applyMiddleware(thunk))
+
+export default store;
